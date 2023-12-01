5770e31ad48f6f65c66168c58dd6ee1fa48efa3e
Roberto <rbarrero@posteo.net>
2023-12-01 23:01:10 +0100

    on my way

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(purchased_apples: i8) -> i8 {
+    let mut price = 2;
+    if purchased_apples > 40 {
+        price = 1;
+    }
+    purchased_apples * price
+}
 
 // Don't modify this function!
 #[test]
